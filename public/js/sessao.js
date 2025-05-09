a8d02d8b91ef770de58e123f265c0f15c025035e
Davi Silva <davi.fsilva@sptech.school>
2025-05-08 21:10:35 -0300

    feat: add field with pre-filled cpf with help based on active session

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -2,11 +2,14 @@
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
+    var cpf = sessionStorage.CPF_USUARIO;
 
     var b_usuario = document.getElementById("b_usuario");
+    var c_usuario = document.getElementById("c_usuario");
 
-    if (email != null && nome != null) {
+    if (email != null && nome != null /*&& cpf != null*/) {
         b_usuario.innerHTML = nome;
+        c_usuario.innerHTML = cpf;
     } else {
         window.location = "../login.html";
     }
--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -29,6 +29,7 @@ function autenticar(req, res) {
                                         email: resultadoAutenticar[0].email,
                                         nome: resultadoAutenticar[0].nome,
                                         senha: resultadoAutenticar[0].senha,
+                                        cpf: resultadoAutenticar[0].cpf,
                                         aquarios: resultadoAquarios
                                     });
                                 } else {
